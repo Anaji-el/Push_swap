06223e942f1b54d78fb391ff255817b91f7d7ae1
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-04-01 05:51:45 +0000

    modified on header

--- push_swap.h
+++ push_swap.h
@@ -6,16 +6,21 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 23:46:17 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/03/24 23:30:06 by anaji-el         ###   ########.fr       */
+/*   Updated: 2022/04/01 05:51:09 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct t_type
+typedef struct	s_type
 {
-	int	stacka;
-	int	stackb;
-	int	algorithm;
+	int	size;
+	int	data;
+	int	array;
+}
+typedef struct	s_stack
+{
+	t_type	stack_a;
+	t_type	stack_b;
 }
