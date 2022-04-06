cc8895a829edf7eec5f90f56476a6ab7726b2648
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-04-06 04:45:37 +0000

    add checker

--- push_swap.h
+++ push_swap.h
@@ -6,21 +6,7 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 23:46:17 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/04/01 05:51:09 by anaji-el         ###   ########.fr       */
+/*   Updated: 2022/04/05 03:04:13 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct	s_type
-{
-	int	size;
-	int	data;
-	int	array;
-}
-typedef struct	s_stack
-{
-	t_type	stack_a;
-	t_type	stack_b;
-}
