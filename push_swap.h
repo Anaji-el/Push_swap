18f843aab1d5672fefc4c37311191445e4e0ed2c
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-04-14 05:32:10 +0000

    add header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,19 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 23:46:17 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/04/14 05:23:32 by anaji-el         ###   ########.fr       */
+/*   Updated: 2022/04/14 05:30:03 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int	ft_atoi(const char *str);
+
+#endif
