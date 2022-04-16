99909c493a6a41374abcfaa88dbafea6d69cbb2a
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-04-16 05:44:36 +0000

    modified on struct

--- sorted.c
+++ sorted.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   sorted.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 23:46:17 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/04/16 05:43:35 by anaji-el         ###   ########.fr       */
+/*   Created: 2022/04/15 04:56:19 by anaji-el          #+#    #+#             */
+/*   Updated: 2022/04/15 04:57:36 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct	node;
+int	sorted(int	stack)
 {
-	int	number;
-	struct node	*next;
-}t_node;
-
-
-int	ft_atoi(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
-
-#endif
+	
+}
