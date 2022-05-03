d8ad2be682d401bd02d5e6fcbc31ac6f8a7c951e
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-05-03 20:42:39 +0000

    add r & rr

--- sorting.c
+++ sorting.c
@@ -6,20 +6,46 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 06:48:00 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/05/01 07:01:23 by anaji-el         ###   ########.fr       */
+/*   Updated: 2022/05/03 20:33:15 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_node *stack)
+void	swap(t_node *stack, char *str)
 {
 	int	tmp;
 	
-	if (!stack || !stack->next)
+	if (!stack || !(stack->next))
 		return ;
 	tmp = stack->data;
 	stack->data = stack->next->data;
 	stack->next->data = tmp;
+	if (str)
+		ft_putstr(str);
 }
 
+void	rotate(t_node *stack, char *str)
+{
+	if (!stack || !(stack->next))
+	return ;
+	while (stack)
+	{
+		swap(stack, 0);
+		stack = stack->next;
+	}
+	if (str)
+		ft_putstr(str);
+}
+
+void	r_rotate(t_node *stack, char *str)
+{
+	if (!stack || !(stack->next))
+		return ;
+	r_rotate(stack->next, 0);
+	swap(stack, 0);
+	if (!str)
+		return ;
+	if (str)
+		ft_putstr(str);
+}
