0c1d4195aa646e1b22e56c8fbfe1d5a7232cfe6d
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-05-04 21:51:25 +0000

    add sort three

---
+++ sort_three.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 21:45:03 by anaji-el          #+#    #+#             */
+/*   Updated: 2022/05/04 21:51:01 by anaji-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_three(t_node **a, int size)
+{
+	if (sorted(*a))
+		return ;
+	if (size == 3)
+	{
+		if (max == (*a)->data)
+			rotate(*a, "ra");
+			else if (max == (*a)->next->data)
+				r_rotate(*a, "rra");
+	}
+	if ((*a)->data > (*a)->next->data)
+		swap(*a, "sa");
+}
